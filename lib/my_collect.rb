f99725fd548a91856fb7f45fc155d6da531a56b7
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-14 02:26:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
-
-
+def my_collect(empty_array)
+  i = 0
+  while i < empty_array.length
+    yield(empty_array[i])
+    i += 1
+    
+  end
+  
+  if empty_array.length == 0 
+    return empty_array
+  else
+      languages = empty_array.join(" ")
+      capital_lang = languages.upcase
+      new_lang_array = capital_lang.split(" ")
+      return new_lang_array
+    
+  end
+  
+end
